f5cf9e31eff71b3cbcf5b129cd32b77bbef0d270
JoshTangendjaja <joshtangendjaja@gmail.com>
2024-04-03 17:08:44 +1100

    Update bedroom.js
    Include base image loading from reactnative
    
    Waiting on Asset folder

--- front-end/Bedroom/bedroom.js
+++ front-end/Bedroom/bedroom.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
-import { Button, TextInput, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {ImageBackground, Button} from 'react-native';
+
+const image = <Image source={require('./bedroombackground.png')} />
 
 const BedroomScreen = () => {
-    
+    <View style={backgroundStyle.container}>
+        <ImageBackground source={image} resizeMode="cover" style={backgroundStyle.image}>
+        </ImageBackground>
+  </View>
 };
+
+const backgroundStyle = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'center',
+    }
+  });
