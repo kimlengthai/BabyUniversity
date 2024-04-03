826c01d5834be833475888515966c728b2d0a18b
JoshTangendjaja <joshtangendjaja@gmail.com>
2024-04-03 17:38:55 +1100

    Create bookshelf.js
    Bookshelf Folder

---
+++ front-end/BookShelf/bookshelf.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import {ImageBackground, Button} from 'react-native';
+
+const image = <Image source={require('./bookshelfbackground.png')} />
+
+const BookshelfScreen = () => {
+    <View style={backgroundStyle.container}>
+        <ImageBackground source={image} resizeMode="cover" style={backgroundStyle.image}>
+        </ImageBackground>
+  </View>
+};
+
+const backgroundStyle = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'center',
+    }
+  });
