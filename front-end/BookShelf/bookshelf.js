49ea8acf8e91ec322f96c439de42fef75dc2a647
JoshTangendjaja <joshtangendjaja@gmail.com>
2024-05-01 15:45:23 +1000

    Update bookshelf.js

--- front-end/BookShelf/bookshelf.js
+++ front-end/BookShelf/bookshelf.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
-import {ImageBackground, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground, Button, Image } from 'react-native';
 
 const image = <Image source={require('./bookshelfbackground.png')} />
 
 const BookshelfScreen = () => {
+  return(
     <View style={backgroundStyle.container}>
-        <ImageBackground source={image} resizeMode="cover" style={backgroundStyle.image}>
-        </ImageBackground>
-  </View>
+        <Image 
+        source={require('../assets/bedRoomImages/background_doodle.png')} //placeholder atm no bookshelf 
+        style={styles.image} 
+        />
+    </View>
+  )
 };
 
 const backgroundStyle = StyleSheet.create({
     container: {
       flex: 1,
+      position: 'relative',
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     image: {
       flex: 1,
+      position: 'absolute',
       justifyContent: 'center',
+      resizeMode: 'contain',
+      top: 0
     }
   });
