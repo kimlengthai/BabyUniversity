aba3ec7d09e8a0f5f78f27e1f7d01cd32bf0df80
Ali-jfry <alijfry029@gmail.com>
2024-05-08 14:50:30 +1000

    Animations for page 6 - 12
    added the animation for page 6 -12

--- front-end/App.js
+++ front-end/App.js
@@ -18,6 +18,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import BedRoomScreen from './Bedroom/BedRoomScreen';
+import page6 from './page6-17/Page6'
+import page7 from './page6-17/Page7'
+import page8 from './page6-17/Page8'
+import page9 from './page6-17/Page9'
+import page10 from './page6-17/Page10'
+import page11 from './page6-17/Page11'
+import page12 from './page6-17/Page12'
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -25,9 +32,20 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
 
-        <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
+        {/* <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
         <Stack.Screen options= {{headerShown: false}} name="Home" component={HomeScreen} />
-        <Stack.Screen options= {{headerShown: false}} name="Bedroom" component={BedRoomScreen} />
+        <Stack.Screen options= {{headerShown: false}} name="Bedroom" component={BedRoomScreen} /> */}
+
+        {/* book pages 6-17 */}
+
+        {/* <Stack.Screen options= {{headerShown: false}} name="page6" component={page6} />  */}
+        {/* <Stack.Screen options= {{headerShown: false}} name="page7" component={page7} />  */}
+        {/* <Stack.Screen options= {{headerShown: false}} name="page8" component={page8} />  */}
+        {/* <Stack.Screen options={{headerShown: false}} name="page9" component={page9} /> */}
+        {/* <Stack.Screen options= {{headerShown: false}} name="page10" component={page10} />  */}
+        {/* <Stack.Screen options= {{headerShown: false}} name="page11" component={page11} /> */}
+        <Stack.Screen options= {{headerShown: false}} name="page12" component={page12} />
+
 
 
       </Stack.Navigator>
