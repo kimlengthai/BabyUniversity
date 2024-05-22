8e97528dd0dac30b0172df4fe9442e48880388fa
Abhishek Chand <abhishekc117@hotmail.com>
2024-05-22 22:04:18 +1000

    audio
    main branch plus audio for the book pages with expo av. Do 'npx expo install expo-av' in front-end dir before trying to run

--- front-end/BookPages/Page8.js
+++ front-end/BookPages/Page8.js
@@ -2,33 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { View, Animated, StyleSheet, Easing, Text } from 'react-native';
 
 
-import CircleAnimation from '../page6-17/components/CircleAnimation';
+import CircleAnimation from '../BookPages/components/CircleAnimation';
 export default function Page8() {
   return (
-    <View style = {styles.container}>
-      <CircleAnimation  />
+    <View style={styles.container}>
+      <CircleAnimation />
 
       {/* text */}
-      <View style = {styles.text}>
-        <Text style = {{color: 'white', fontSize: 50}}>And <Text style ={{color: 'green'}}>electrons.</Text></Text>
+      <View style={styles.text}>
+        <Text style={{ color: 'white', fontSize: 50 }}>And <Text style={{ color: 'green' }}>electrons.</Text></Text>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'relative',
-        backgroundColor: 'black',
-    },
-    text:{
-        position: 'absolute',
-        zIndex: 100,
-        bottom: 50,
-        fontWeight: '700',
-      
-      }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    backgroundColor: 'black',
+  },
+  text: {
+    position: 'absolute',
+    zIndex: 100,
+    bottom: 50,
+    fontWeight: '700',
+
+  }
 })
