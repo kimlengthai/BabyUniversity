a9add3d34ecee12ab99868c80928c62fd53e8285
td2004 <tdarpitha2004@gmail.com>
2024-05-24 16:02:37 +1000

    fix for ipad

--- front-end/BookStore/Payment.js
+++ front-end/BookStore/Payment.js
@@ -111,81 +111,70 @@ const Payment = () => {
     <KeyboardAvoidingView style={styles.container} behavior='padding'>
       <Text style={styles.header}>The Book Store</Text>
       <View>
-        <Image source={phyDoodleShapes} style={styles.backgroundImage} />
-        <Text style={styles.AiBookTitle}>Artificial Intelligence for Babies</Text>
-        <View style={styles.paymentContainer}>
-          <View style={styles.cardContainer}>
-            <Text style={styles.label}>Card Number: </Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your card number"
-              value={cardNumber}
-              onChangeText={setCardNumber}
-              keyboardType="numeric"
-            />
-          </View>
-          {cardNumberError ? <Text style={styles.error}>{cardNumberError}</Text> : null}
+      <Image source={phyDoodleShapes} style={[styles.backgroundImage]} />
+      <Text style={styles.AiBookTitle}>Artificial Intelligence for Babies</Text>
+      <View style = {styles.paymentContainer}>
 
-          <View style={styles.numberContainer}>
-            <Text style={styles.label}>Expiry Date:</Text>
-            <TextInput
-              style={styles.inputDate}
-              placeholder="Enter your expiry date"
-              value={expiryDate}
-              onChangeText={setExpiryDate}
-              keyboardType="numeric"
-            />
-            {expiryDateError ? <Text style={styles.error}>{expiryDateError}</Text> : null}
+      <View style={styles.cardContainer}>
+      <Text style={styles.label}>Card Number: </Text>
+      <TextInput
+                        style={styles.input}
+                        placeholder="Enter your card number"
+                    />
+      </View>
 
-            <Text style={styles.labelCVV}>CVV:</Text>
-            <TextInput
-              style={styles.inputCVV}
-              placeholder="Enter your CVV"
-              value={cvv}
-              onChangeText={setCvv}
-              keyboardType="numeric"
-              secureTextEntry
-            />
-            {cvvError ? <Text style={styles.error}>{cvvError}</Text> : null}
-          </View>
+      <View style={styles.numberContainer}>
+      <Text style={styles.label}>Expiry Date:</Text>
+      <TextInput
+                        style={styles.inputDate}
+                        placeholder="Enter your expiry date"
+                    />
+      <Text style={styles.labelCVV}>CVV:</Text>
+      <TextInput
+                        style={styles.inputCVV}
+                        placeholder="Enter your CVV"
+                    />
+      </View>
 
-          <View style={styles.nameContainer}>
-            <Text style={styles.label}>Name on Card:</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your card name"
-              value={name}
-              onChangeText={setName}
-            />
-            {nameError ? <Text style={styles.error}>{nameError}</Text> : null}
-          </View>
-        </View>
-        <Image source={AiBook} style={styles.AiBookImg} />
-        <Text style={styles.pricetag}>$12.99</Text>
+      <View style={styles.nameContainer}>
+      <Text style={styles.label}>Name on Card:</Text>
+      <TextInput
+                        style={styles.input}
+                        placeholder="Enter your card name"
+                    />
+      </View>
 
-        <TouchableOpacity style={styles.buyButtonIcon} onPress={handleConfirmPurchase}>
-          <Image source={ConfirmPurchaseButton} style={styles.ConfirmPurchaseButton} />
-        </TouchableOpacity>
+      </View>
+      <Image source={AiBook} style={[styles.AiBookImg]} />
+      <Text style={styles.pricetag}>$12.99</Text>
+
+      <TouchableOpacity style={styles.buyButtonIcon} onPress={handleConfirmPurchase}>
+      <Image source={ConfirmPurchaseButton} style={[styles.ConfirmPurchaseButton]} />
+      </TouchableOpacity>
       </View>
 
       <TouchableOpacity style={styles.goBackIcon} onPress={handleGoBack}>
-        <Image source={goBackButton} style={styles.goBack} />
+        <Image source={goBackButton} style={styles.goBack}/>
       </TouchableOpacity>
+      
     </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container: 
+  {
     flex: 1,
     backgroundColor: '#E0F6FF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  paymentContainer: {
+  paymentContainer:
+  {
     width: '50%',
   },
-  cardContainer: {
+  cardContainer:
+  {
     width: '100%',
     display: 'flex',
     flexDirection: 'row',
@@ -194,7 +183,8 @@ const styles = StyleSheet.create({
     left: 60,
     top: -30,
   },
-  numberContainer: {
+  numberContainer:
+  {
     width: '100%',
     display: 'flex',
     flexDirection: 'row',
@@ -202,7 +192,8 @@ const styles = StyleSheet.create({
     left: 60,
     top: -10,
   },
-  nameContainer: {
+  nameContainer:
+  {
     width: '100%',
     display: 'flex',
     flexDirection: 'row',
@@ -215,93 +206,112 @@ const styles = StyleSheet.create({
     marginRight: 250,
     bottom: 20,
     fontSize: 30,
-    fontFamily: 'Itim_400Regular',
+    fontFamily: "Itim_400Regular",
   },
-  labelCVV: {
+  labelCVV: 
+  {
     marginRight: 20,
     right: 123,
     fontSize: 30,
-    fontFamily: 'Itim_400Regular',
+    fontFamily: "Itim_400Regular",
   },
-  input: {
+  input:
+  {
     backgroundColor: '#FFFFFF',
-    padding: 20,
+    paddingHorizontal: 10, // Reduced padding
+    paddingVertical: 5,
     width: 525,
-    height: 43,
+    height: 50, // Increased height
     borderRadius: 14,
     right: 230,
     bottom: 15,
+    fontSize: 18, // Adjust font size if needed
   },
-  inputDate: {
+  inputDate:
+  {
     backgroundColor: '#FFFFFF',
-    padding: 20,
+    paddingHorizontal: 10, // Reduced padding
+    paddingVertical: 5,
     right: 190,
     borderRadius: 14,
     width: 183,
-    height: 43,
+    height: 50, // Increased height
     bottom: 5,
+    fontSize: 18, // Adjust font size if needed
   },
-  inputCVV: {
+  inputCVV:
+  {
     backgroundColor: '#FFFFFF',
     width: 183,
-    height: 43,
+    height: 50, // Increased height
     borderRadius: 14,
-    padding: 20,
+    paddingHorizontal: 10, // Reduced padding
+    paddingVertical: 5,
     right: 120,
+    fontSize: 18, // Adjust font size if needed
   },
-  header: {
+  header:
+  {
     fontSize: 50,
-    fontFamily: 'Itim_400Regular',
+    fontFamily: "Itim_400Regular",
     color: '#3F3CB4',
     top: 125,
     left: 5,
   },
-  AiBookTitle: {
-    fontSize: 30,
-    fontFamily: 'Itim_400Regular',
+  AiBookTitle:
+  {
+    fontSize: 40,
+    fontFamily: "Itim_400Regular",
     color: '#ED5D5B',
     bottom: 45,
     left: 60,
-    width: 300,
-    height: 36,
+    width: 550,
+    height: 48,
   },
-  AiBookImg: {
-    left: 200,
-    bottom: 200,
-    width: 150,
-    height: 150,
+  AiBookImg:
+  {
+    left: 865,
+    bottom: 275,
+    width: 254,
+    height: 254,
   },
-  pricetag: {
-    fontSize: 25,
-    fontFamily: 'Itim_400Regular',
+  pricetag:
+  {
+    fontSize: 30,
+    fontFamily: "Itim_400Regular",
     color: '#3D3AAF',
     width: 84,
     height: 36,
-    bottom: 220,
-    left: 240,
+    bottom: 260,
+    left: 950,
   },
-  ConfirmPurchaseButton: {
-    bottom: 100,
-    left: 200,
-    width: 150,
-    height: 50,
+  ConfirmPurchaseButton:
+  {
+    bottom: 70,
+    left: 840,
   },
-  backgroundImage: {
-    position: 'absolute',
-    width: '100%',
+  backgroundImage: 
+  {
+    flex: 1,
+    justifyContent: 'center',
     height: '100%',
-    top: 0,
-    left: 0,
+    marginTop: 10,
+    marginBottom: -600,
   },
-  goBackIcon: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
+  goBackIcon:
+  {
+    color: '#292D32',
+    backgroundColor: 'transparent',
+    bottom: 800,
+    right: 535,
+    zIndex: 1,
   },
-  goBack: {
+  goBack:
+  {
     width: 77,
     height: 77,
   },
+
   error: {
     color: 'red',
     marginTop: 10,
