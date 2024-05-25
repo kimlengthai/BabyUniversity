8472d0441437e1978ccc4f0eb22464aafee97044
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-05-25 20:10:18 +1000

    Added bg colour + font family
    I only added the background, its colour, and font family, and changed the font size of the prompt text.

--- front-end/PinPage.js
+++ front-end/PinPage.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import axios from 'axios';
 import { auth } from './firebase'; // Adjust the path based on your firebase setup
 import ParentUI from './ParentUI/ParentUI';
+import phyDoodleShapes from '../front-end/assets/BgImage/doodle.png';
+import { useFonts, Itim_400Regular } from '@expo-google-fonts/itim';
 
 const PinEntryScreen = ({ navigation }) => {
   const [pin, setPin] = useState('');
   const [error, setError] = useState('');
+  let [fontsLoaded] = useFonts({ Itim_400Regular });
 
   const handlePinChange = (enteredPin) => {
     setPin(enteredPin);
@@ -32,9 +35,13 @@ const PinEntryScreen = ({ navigation }) => {
       setError('Incorrect PIN. Please try again.');
     });
   };
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
 
   return (
     <View style={styles.container}>
+    <Image source={phyDoodleShapes} style={[styles.backgroundImage]} />
       <Text style={styles.errorText}>{error}</Text>
       <Text style={styles.prompt}>Enter 4-Digit PIN:</Text>
       <TextInput
@@ -44,7 +51,9 @@ const PinEntryScreen = ({ navigation }) => {
         keyboardType="numeric"
         maxLength={4}
       />
-      <Button title="Submit" onPress={handlePinSubmit} />
+      <View style={styles.submitButtonContainer}>
+        <Button title="Submit" onPress={handlePinSubmit} />
+      </View>
       <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.goBack()}>
         <Text style={styles.goBackText}>Go Back</Text>
       </TouchableOpacity>
@@ -57,15 +66,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#E0F6FF',
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+    height: '100%',
+    marginTop: 0,
+    marginBottom: -200,
   },
   errorText: {
     color: 'red',
     marginBottom: 10,
   },
   prompt: {
-    fontSize: 18,
+    fontSize: 30,
     marginBottom: 20,
+    fontFamily: 'Itim_400Regular',
+    color: '#3F3CB4',
+    bottom: 350,
   },
   input: {
     width: '80%',
@@ -75,9 +94,16 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
     fontSize: 24,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 14,
+    bottom: 350,
+  },
+  submitButtonContainer: {
+    bottom: 300,
   },
   goBackButton: {
     marginTop: 20,
+    bottom: 300,
   },
   goBackText: {
     color: '#3F3CB4',
