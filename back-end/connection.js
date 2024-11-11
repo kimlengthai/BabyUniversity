ac1795c9a460e38641fabceaffe1310bb3b7df79
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-11-11 12:39:08 +1100

    Add .env to .gitignore

--- back-end/connection.js
+++ back-end/connection.js
@@ -29,12 +29,12 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCFTiMrSAqmP_73arZ7pNxo0ruGGYNlPdw",
-  authDomain: "baby-university-19691.firebaseapp.com",
-  projectId: "baby-university-19691",
-  storageBucket: "baby-university-19691.appspot.com",
-  messagingSenderId: "406813890694",
-  appId: "1:406813890694:web:4bf6d73f00037713f91907"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
